fe12f19060f3912117cb7048e9bee03506a3f498
byron7cueva <byron7cueva@gmail.com>
2020-08-23 07:16:46 -0500

    Template Literals

---
+++ ecmascript/src/es6/template-literals.js
@@ -0,0 +1,11 @@
+// * Template Literals
+
+// antes
+var hello = 'Hello';
+var world = 'World';
+var epicPhrase = hello + ' ' + world;
+console.log(epicPhrase);
+
+// es6
+var epicPhraseEs6 = `${hello} ${world}`;
+console.log(epicPhraseEs6);

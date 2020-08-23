19270fc048dcbe2070f07c16021662960d7fbce6
byron7cueva <byron7cueva@gmail.com>
2020-08-23 15:18:22 -0500

    Object.fromEntries

---
+++ ecmascript/src/es10/from-entries.js
@@ -0,0 +1,6 @@
+// * Object.fromEntries
+// Transforma clave valor a un objeto.
+
+let entries = [['name', 'Byron'], ['age', 32]];
+console.log(Object.fromEntries(entries));
+// Devuelve { name: 'Byron', age: 32 }

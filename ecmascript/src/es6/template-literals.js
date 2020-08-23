8e746b4b72fe566414f67f0d438b397566253387
byron7cueva <byron7cueva@gmail.com>
2020-08-23 07:23:42 -0500

    Multilinea

--- ecmascript/src/es6/template-literals.js
+++ ecmascript/src/es6/template-literals.js
@@ -9,3 +9,18 @@ console.log(epicPhrase);
 // es6
 var epicPhraseEs6 = `${hello} ${world}`;
 console.log(epicPhraseEs6);
+
+
+
+// * Multilinea
+
+// antes
+var lorem = 'Various versions have evolved over \n'
++ 'the years, sometimes by accident';
+
+// es6
+var loremEs6 = `Various versions have evolved over
+the years, sometimes by accident`;
+
+console.log(lorem);
+console.log(loremEs6);

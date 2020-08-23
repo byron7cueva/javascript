fdd6f2e2177d746f7e49dd2c9366a4837b9b30c6
byron7cueva <byron7cueva@gmail.com>
2020-08-23 15:12:18 -0500

    trim

---
+++ ecmascript/src/es10/trim.js
@@ -0,0 +1,10 @@
+// * trimStart
+// Permite eliminar los pespacios en blanco del inicio de una cadena
+
+let hello = '     hello world';
+console.log(hello)
+console.log(hello.trimStart());
+
+let hello2 = 'hello world       ';
+console.log(hello2);
+console.log(hello2.trimEnd());

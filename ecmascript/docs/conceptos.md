dc0df7381832e9d0fad14ac27f530acd4efc46e3
byron7cueva <byron7cueva@gmail.com>
2020-08-23 11:11:56 -0500

    Includes

--- ecmascript/docs/conceptos.md
+++ ecmascript/docs/conceptos.md
@@ -6,3 +6,5 @@
 * Estas fueron lanzdas año por año a partir del 2015 que fue la lanzada la versión 6.
 * 1996 hasta junio del 2001 hasta donde se hizo una revición.
 * Se saca una nueva versión cada junio.
+* es6 Junio del 2015
+* es7 Junio del 2016
---
+++ ecmascript/src/es7/includes.js
@@ -0,0 +1,11 @@
+// * Includes
+// Trabaja sonbre un arreglo o un string
+// Nos permite saber si hay un elemento dentro de estos valores
+
+let numbers = [1, 2, 3, 7, 8];
+
+if (numbers.includes(7)) {
+  console.log('Si se encuentra el valor 7');
+} else {
+  console.log('No se encuentra');
+}

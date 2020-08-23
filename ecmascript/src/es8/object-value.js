ccbf1b0d1345ec8e21e36a36b4cd47b7aaa59cb6
byron7cueva <byron7cueva@gmail.com>
2020-08-23 12:04:17 -0500

    Object.values

---
+++ ecmascript/src/es8/object-value.js
@@ -0,0 +1,13 @@
+// * Object.values
+// Devuelve un arragle con los valores
+// que tengo en un objeto
+
+const data = {
+  frontend: 'Byron',
+  backend: 'David',
+  design: 'Gaby',
+  age: 27
+}
+
+const values = Object.values(data);
+console.log(values);

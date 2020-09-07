0fdb327549436736d916ff64a27be5ab23886234
byron7cueva <byron7cueva@gmail.com>
2020-09-07 11:44:41 -0500

    profesional - Add Ads

--- profesional/website-ts/assets/MediaPlayer.ts
+++ profesional/website-ts/assets/MediaPlayer.ts
@@ -1,24 +1,39 @@
 class MediaPlayer {
-  media: HTMLMediaElement;
+  media: HTMLMediaElement | undefined;
   plugins: Array<any>;
+  container: HTMLElement;
 
-  constructor({ el, plugins = [] } = {}) {
+  constructor({ el = undefined, plugins = [] } = {}) {
     this.media = el;
     this.plugins = plugins;
 
+    this.initPlayer();
     this.initPlugins();
   }
+
   private initPlugins() {
     this.plugins.forEach(plugin => {
       plugin.run(this);
     });
   }
+
+  private initPlayer() {
+    this.container = document.createElement('div');
+    this.container.style.position = 'relative';
+    // Agregando contenedor a lado de media
+    this.media.parentNode.insertBefore(this.container, this.media);
+    // media dentro del contenedor
+    this.container.appendChild(this.media);
+  }
+
   play() {
     this.media.play();
   }
+
   pause() {
     this.media.pause();
   }
+
   togglePlayPuse() {
     if (this.media.paused) {
       this.play();
@@ -27,12 +42,15 @@ class MediaPlayer {
       this.pause();
     }
   }
+
   mute() {
     this.media.muted = true;
   }
+
   unmute() {
     this.media.muted = false;
   }
+
   toggleMute() {
     if (this.media.muted) {
       this.unmute();
--- profesional/website-ts/assets/index.ts
+++ profesional/website-ts/assets/index.ts
@@ -1,6 +1,7 @@
 import MediaPlayer from './MediaPlayer';
 import AutoPlay from './plugins/AutoPlay';
 import AutoPause from './plugins/AutoPause';
+import Ads from './plugins/Ads'
 
 const video: HTMLMediaElement = document.querySelector('video');
 const playButton: HTMLElement = document.getElementById('playButton');
@@ -10,15 +11,16 @@ const player = new MediaPlayer({
   el: video,
   plugins: [
     new AutoPlay(),
-    new AutoPause()
+    new AutoPause(),
+    new Ads()
   ]
 });
 
 playButton.onclick = () => player.togglePlayPuse();
 muteButton.onclick = () => player.toggleMute();
 
-if('serviceWorker' in navigator) {
+/* if('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js').catch(error => {
     console.error(error.message);
   });
-}
+} */
